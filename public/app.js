03b9333fb2c4c26503c53679dcb43e159aff5bb0
Simon Miner <sminer@christianbook.com>
2016-07-17 16:17:25 -0400

    Create initial simple data model

--- public/app.js
+++ public/app.js
@@ -2,6 +2,17 @@
 
 var learnjs = {};
 
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem() { return 42 === 6 * __; `}"
+  }
+];
+
 learnjs.appOnReady = function() {
   window.onhashchange = function() {
     learnjs.showView(window.location.hash);
@@ -9,9 +20,17 @@ learnjs.appOnReady = function() {
   learnjs.showView(window.location.hash);
 }
 
-learnjs.problemView = function(problemNumber) {
+learnjs.applyObject = function(obj, elem) {
+  for (var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+};
+
+learnjs.problemView = function(data) {
+  var problemNumber = parseInt( data, 10 );
   var view = $('.templates .problem-view').clone();
   view.find('.title').text('Problem #' + problemNumber);
+  learnjs.applyObject( learnjs.problems[ problemNumber - 1 ], view );
   return view;
 }
 
