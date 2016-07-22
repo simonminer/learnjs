a3701ff7ced280eee4e38f2fab11c92d7d0d7e04
Simon Miner <sminer@christianbook.com>
2016-07-22 07:58:19 -0400

    Create tests for flash element

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -47,16 +47,25 @@ describe('LearnJS', function() {
        expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
     });
 
-    it('can check a correct answer by hitting a button', function() {
-      view.find('.answer').val('true');
-      view.find('.check-btn').click();
-      expect(view.find('.result').text()).toEqual('Correct!');
-    });
+    describe( 'answer section', function () {
+      var resultFlash;
 
-    it('rejects an incorrect answer', function() {
-      view.find('.answer').val('false');
-      view.find('.check-btn').click();
-      expect(view.find('.result').text()).toEqual('Incorrect!');
-    });  
+      beforeEach(function() {
+        spyOn(learnjs, 'flashElement');
+        resultFlash = view.find('.result');
+      });
+
+      it('can check a correct answer by hitting a button', function() {
+        view.find('.answer').val('true');
+        view.find('.check-btn').click();
+        expect(learnjs.flashElement).toHaveBeenCalledWith(resultFlash, 'Correct!');
+      });
+
+      it('rejects an incorrect answer', function() {
+        view.find('.answer').val('false');
+        view.find('.check-btn').click();
+        expect(learnjs.flashElement).toHaveBeenCalledWith(resultFlash, 'Incorrect!');
+      });  
+    });
   });
 });
